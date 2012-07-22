f369aa6a9e173d3e499c13c0b0e3361514c44188
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2012-07-22 00:02:01 -0300

    atualização

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-     @galleries = Gallery.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @galleries }
-    end
+     @galleries = Gallery.all  
     
   end
 end
