608855d0388cf46272ac652862feddbaf634ce5c
Erlon Carlos de Oliveira Gomes <erlonlon@gmail.com>
2012-07-29 21:41:48 -0300

    validação de campos

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
   attr_accessible :assunto, :conteudo, :data_contato, :email, :nome
+  
+  validates_presence_of :assunto, :conteudo, :email, :nome
+  validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,11 +1,13 @@
+#encoding: utf-8
 class Gallery < ActiveRecord::Base
   attr_accessible :nome, :foto
   
-  has_attached_file :foto, :styles => {:medium => "300x300" , :thumb => "166x130" , :original => "940x340"}
+  has_attached_file :foto, :styles => {:medium => "300x300" , :thumb => "166x130", }
   
- 
-#scope :recent, limit(3).order("created_at ASC")
- #Gallery.reorder("data_cadastro ASC").limit(2)
- 
- #Galllery.all(:order => "nome ASC", :limit => 2)
+
+ validates_attachment_content_type :foto, :content_type => ["image/jpg", "image/jpeg"]
+# diz o tipo de extensão que o sistema aceita para validar o upload.
+
+ validates_attachment_size :foto, :less_than => 1.megabyte
+
 end
